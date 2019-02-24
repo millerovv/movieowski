852ef6f149ecf852da7bfb8789d2d7f1da50be88
Александр Мельников <melnikov.ws@gmail.com>
2019-02-24 19:11:22 +0300

    Sample commit 2.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  //sample change
+  //sample change 2
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
