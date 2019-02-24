7859c4ded6746941f50ef9ba339f2ed69cc22afc
Александр Мельников <melnikov.al.se@sberbank.ru>
2019-02-24 19:06:55 +0300

    Sample commit.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  //sample change
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
